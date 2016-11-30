85aba556bb526443c68a95768bb5a089e60d8c0f
Meradana <meradana@hotmail.se>
2016-11-30 09:44:03 +0100

    Lade in slutförd uppgift

--- Lab 2/sieves.c
+++ Lab 2/sieves.c
@@ -1,39 +1,58 @@
-input: an integer n > 1
+/*
+ sieves.c
+ Written By Niklas Bergdahl
+ 30/09-2016
+*/
 
-Let A be an array of Boolean values, indexed by integers 2 to n,
-initially all set to true.
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <time.h>
 
-for i = 2, 3, 4, ..., not exceeding √n:
-  if A[i] is true:
-    for j = i2, i2+i, i2+2i, i2+3i, ..., not exceeding n :
-      A[j] := false
+#define COLUMNS 6
+int printAmount = 0;
 
-Output: all i such that A[i] is true.
-
-
-
-
-or i = 2, 3, 4, ..., not exceeding √n:
-  if A[i] is true:
-    for j = 2i,3i,4i, i2+3i, ..., not exceeding n :
-      A[j] := false
-
-
-if (n < 2){
-  printf("%s\n", "Error, number has to be greater than 1" );
-
-}
-  else{
-    bool barray[n];
-      for (int i = 0; i <= n; i++){
-          barray[i] = true;
-        }
-
-    for (int i = 0; i*i <= n; i++){
-       if (barray[i] == true){
-         for (int j = 2i; j <= n; j+=i;
-           barray[j] = false;
-
-       }
+void print_number(int n) {
+    if (printAmount % COLUMNS == 0){
+        printf("\n");
     }
+    printf("%10d ", n);
+    printAmount++;
+}
+
+void print_sieves(int n){
+    if(n < 2){
+        printf("%s\n", "Error, number has to be greater than 1" );
+    } else {
+        bool barray[n-1]; //skapar en array som går från 0-(n-2)
+        for(int i = 0; i < n-1; i++){
+            barray[i] = true;
+        }
+        for(int i = 0; (i+2)*(i+2) <= n; i++){
+            if(barray[i] == true){
+                for(int j = (i+2)*(i+2); j <= n; j += (i+2)) {
+                    barray[j-2] = false;
+                }
+            }
+        }
+        for(int i = 0; i < n-1; i++) {
+            if(barray[i] == true) {
+                print_number(i+2);
+            }
+        }
+    }
+}
+// 'argc' contains the number of program arguments, and
+// 'argv' is an array of char pointers, where each
+// char pointer points to a null-terminated string.
+int main(int argc, char *argv[]){
+  clock_t begin = clock();
+  if(argc == 2){
+    print_sieves(atoi(argv[1]));
+    clock_t end = clock();
+    printf("time elapsed: %f seconds\n",(double)(end-begin)/CLOCKS_PER_SEC);
   }
+  else
+    printf("Please state an interger number.\n");
+  return 0;
+}
